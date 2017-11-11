8d46de66132eb9deef4e0da65f37336af4eff7b4
VSBalapure <32739965+VSBalapure@users.noreply.github.com>
2017-11-11 16:33:52 +0530

    VSBalapure Programming Assignment 2: Lexical Scoping
    My coursera Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  This is a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## This function takes matrix as input and carry out hte inverse and stores in
+## Cache
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix())
+{
+    invrs <- NULL
+    setit <- function(y) 
+    {
+        x <<- y
+        invrs <<- NULL
+    }
+    getit <- function() x
+    
+    setInver <- function(inverse) invrs <<- inverse
+    
+    getInver <- function() invrs
+    
+    list(setit = setit,
+         getit = getit,
+         setInver = setInver,
+         getInver = getInver)
 
 }
 
 
-## Write a short comment describing this function
+## This funcion first check if the inverse of the supplied matrix is already 
+## calculated and stored in the cache if yes then pull the result from cache 
+## else calculate the same 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)
+    {
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInvers()
+    if (!is.null(invrs))
+    {
+        message("Retriving cached data")
+        return(invrs)
+    }
+    mat <- x$getit()
+    invrs <- solve(mat, ...)
+    x$setInvers(invrs)
+    invrs
 }
